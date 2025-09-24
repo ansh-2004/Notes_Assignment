02605abf2ca56169463ddcba259ea6025701dc79
Gupta <ansh.gupta@innovasolutions.com>
2025-09-24 17:32:00 +0530

    langchain

--- GenAI/LangChain/projects/01_project/ChatModels/chatModel_openai.js
+++ GenAI/LangChain/projects/01_project/ChatModels/chatModel_openai.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const model = new ChatOpenAI({
   apiKey: process.env.OPENAI_API_KEY,
-  modelName: "gpt-4",
+  model: "gpt-4",
   // temperature: 0.9,
   // maxTokens: 1000,
 });
@@ -19,6 +19,6 @@ console.log("OpenAI Response:", response.content);
 
 
 // temperature :   it is use to make the response more creative , if you want more creative response then you can set temperature to 0.9 or 1.0 , if you want more precise response then you can set temperature to 0.0 or 0.1
-// maxTokens : it is use to limit the number of tokens in the response , if you want more detailed response then you can set maxCompletionTokens to 1000 or 2000 , if you want more concise response then you can set maxCompletionTokens to 100 or 200
+// maxTokens : it is use to limit the number of tokens in the response , if you want more detailed response then you can set maxTokens to 1000 or 2000 , if you want more concise response then you can set maxTokens to 100 or 200
 
 
