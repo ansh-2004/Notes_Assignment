924107c707993a13849fd558a327c04a6c29eff8
Gupta <ansh.gupta@innovasolutions.com>
2025-10-08 17:31:41 +0530

    python

--- python/18.py
+++ python/18.py
@@ -152,15 +152,139 @@
 
 # - Searching operation 
 
-import array 
-arr = array.array('i',[1,2,3,1,2,5])
-print(type(arr)) # <class 'array.array'>
-print(arr) # array('i', [1, 2, 3, 1, 2, 5])
-print("The new created array is :",end="")
-for i in range(0,6):
-    print(arr[i],end=" ")  # The new created array is :1 2 3 1 2 5 
-print("\r")
-print("The index of 1st occurrence of 2 is :",end="")
-print(arr.index(2)) # 1
-print("The index of 1st occurrence of 1 is :",end="")
-print(arr.index(1)) # 0
+# import array 
+# arr = array.array('i',[1,2,3,1,2,5])
+# print(type(arr)) # output: <class 'array.array'>
+# print(arr) # output: array('i', [1, 2, 3, 1, 2, 5])
+# print("The new created array is :",end="")
+# for i in range(0,6):
+#     print(arr[i],end=" ")  # output: The new created array is :1 2 3 1 2 5 
+# print("\r")
+# print("The index of 1st occurrence of 2 is :",end="")
+# print(arr.index(2)) # output: 1
+# print("The index of 1st occurrence of 1 is :",end="")
+# print(arr.index(1)) # output: 0
+
+
+#---
+
+# Array Module 
+
+# The `array` module provides an `array()` object that stores elements of the **same data type** in a compact form. It's more efficient than Python lists for numerical data.
+
+### Importing the Module
+# ```python
+# import array
+# ```
+
+ 
+## Creating an Array
+
+# arr = array.array('i', [1, 2, 3, 4])
+
+
+# - `'i'` is the **type code** for signed integers.
+# - Other type codes:
+#   | Type Code | C Type             | Python Type       |
+#   |-----------|--------------------|-------------------|
+#   | `'b'`     | signed char        | int               |
+#   | `'B'`     | unsigned char      | int               |
+#   | `'u'`     | Unicode character  | str (deprecated)  |
+#   | `'h'`     | signed short       | int               |
+#   | `'H'`     | unsigned short     | int               |
+#   | `'i'`     | signed int         | int               |
+#   | `'I'`     | unsigned int       | int               |
+#   | `'f'`     | float              | float             |
+#   | `'d'`     | double             | float             |
+
+## Common Methods
+
+### 1. `append(x)`
+# Adds an element to the end.
+
+# arr.append(5)
+
+
+### 2. `insert(i, x)`
+# Inserts `x` at index `i`.
+
+# arr.insert(2, 10)
+
+
+### 3. `pop([i])`
+# Removes and returns the item at index `i`. If `i` is omitted, removes the last item.
+
+# arr.pop(1)
+
+
+### 4. `remove(x)`
+# Removes the first occurrence of `x`.
+
+# arr.remove(3)
+
+
+### 5. `index(x)`
+# Returns the index of the first occurrence of `x`.
+
+# arr.index(2)
+
+
+### 6. `reverse()`
+# Reverses the array in place.
+
+# arr.reverse()
+
+
+### 7. `buffer_info()`
+# Returns a tuple with memory address and number of elements.
+
+# arr.buffer_info()
+
+
+### 8. `typecode`
+# Returns the type code of the array.
+
+# arr.typecode
+
+
+
+## Iterating Over an Array
+
+
+# for item in arr:
+#     print(item)
+
+
+## Converting Between Array and List
+
+
+# list_version = arr.tolist()
+# new_arr = array.array('i', list_version)
+
+
+ 
+## Array vs List
+
+# | Feature         | `array` Module         | `list`               |
+# |-----------------|------------------------|----------------------|
+# | Type restriction| Same type only         | Mixed types allowed  |
+# | Memory usage    | More efficient         | Less efficient       |
+# | Speed           | Faster for numerics    | Slower               |
+# | Flexibility     | Less flexible          | More flexible        |
+
+
+# If you're working with **large numeric datasets**, the `array` module is a great lightweight alternative to NumPy. 
+
+# ----
+
+# Advantages 
+# - Random access elements 
+# - Easy sorting and iteration 
+# - Replacement fo multiple variables 
+
+# Disadvantages 
+# - Size is fixed 
+# - Difficult to insert and delete 
+# - If capacity is more and occupancy less, most of the array gets wasted. 
+# - Needs contiguous memory 
+
