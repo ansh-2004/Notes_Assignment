a5d929429f865b83379b836abc7e5d6de14a6ee1
Gupta <ansh.gupta@innovasolutions.com>
2025-10-21 17:08:39 +0530

    react

--- react/practice/travellist/src/App.js
+++ react/practice/travellist/src/App.js
@@ -1,20 +1,23 @@
+import { useState } from "react";
 
 
 const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "ticket", quantity: 2, packed: false },
 ];
 
 export default function App() {
   return (
     <div className="app">
       <Logo/>
-      <Form/>
+      <Form initialItems = {initialItems}/>
       <PackingList/>
       <Stats/>
     </div>
   );
 }
+
 
 function Logo(){
   return (
@@ -24,26 +27,102 @@ function Logo(){
   )
 }
 
-function Form(){
+
+function Form({initialItems}){
+
+  const [item,setItem] = useState("")
+  const [quantity,setQuantity] = useState(1)
+
+  function handleSubmit(e){
+    e.preventDefault();
+    initialItems.push(
+      {
+        id : initialItems.length + 1,
+        description : item,
+        quantity : quantity,
+        packed : false
+      }
+    )
+
+  }
   return(
     <div>
      
-     <form className="add-form">
-        <h3>What do you need for your trip</h3>
+     <form className="add-form" onSubmit={(e)=>handleSubmit(e)}>
+        <h3>What do you need for your 😍 trip?</h3>
 
-        <select>
+        <select value={quantity} onChange={(e)=>(setQuantity(e.target.value))}>
          {
-
-          Array.from({ length: 20 }, (_, i) => i+1).map((ele)=>(<option>{ele}</option>))
+          
+          Array.from({ length: 20 }, (_, i) => i+1).map((ele)=>(<option key={ele}>{ele} </option>))
          }
         </select>
-
-        <input placeholder="item..."  ></input>
+        <input placeholder="item..." value={item} onChange={(e)=>(setItem(e.target.value))} ></input>
         <button> ADD </button>
      </form>
       
     </div>
   )
 }
-function PackingList(){}
-function Stats(){}
+
+
+function PackingList(){
+
+  function handleSubmit(){
+
+    alert("Are you sure you want to delete all Items? ")
+
+    console.log("before",JSON.stringify(initialItems))
+    initialItems.splice(0,initialItems.length)
+    console.log("after",JSON.stringify(initialItems))
+  }
+
+
+
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <li key={item.id}>
+            <input type="checkbox" ></input>
+            <span style = {{textDecoration : item.packed ? "line-through" : "none"}}>{item.quantity} {item.description}</span>
+            <button>❌</button>
+          </li>
+        ))}
+      </ul>
+      <div className="actions">
+        <select>
+              <option>Sort By Input Order</option>
+              <option>Sort By Description</option>
+              <option>Sort By Packed Status</option>
+        </select>
+
+        <button onClick={handleSubmit}> Clear List </button>
+      </div>
+    </div>
+  )
+}
+
+
+function Stats(){
+
+  const packedItems = initialItems.reduce((count,obj)=> (obj.packed === true ? count + 1 : count) ,0)
+
+  const packedItemsPercentage = Math.round((packedItems / initialItems.length) * 100)
+  
+
+  return(
+    <div className="stats">
+
+      {
+        initialItems.length === 0 
+        ? (<p>Start adding some items to your packing list 🚀</p>)
+        : packedItems !== initialItems.length 
+        ? (<p>You have {initialItems.length} items on your list, and you already packed {packedItems} ({packedItemsPercentage}%) </p>)
+        : (<p>You got everything! Ready to go ✈️</p>)
+      }
+
+      
+    </div>
+  )
+}
